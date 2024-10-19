a72aac8a14dddc0c57ec63ce039e1dbe722e8b92
Dellrobo4 <techalongs@gmail.com>
2024-10-18 23:10:30 -0700

    Edited which wheels are reversed and changed speed to be outreach-friendly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WalkingFerdinand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WalkingFerdinand.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Walking Ferdinand")
 public class WalkingFerdinand extends LinearOpMode {
-
+ 
     @Override
     public void runOpMode() {
         final double POSITION_ANGLE = 0.6; // Increase = more angled (less steep)
@@ -22,9 +22,9 @@ public class WalkingFerdinand extends LinearOpMode {
                 // Drivetrain controls
                 double y = 0;
 
-                if (gamepad1.right_bumper) ferdinand.drive(1, gamepad1, y); // Standard - 0.84
-                else if (gamepad1.left_bumper) ferdinand.drive(0.7, gamepad1, y); // Standard - 0.4
-                else ferdinand.drive(1, gamepad1, y);
+                if (gamepad1.right_bumper) ferdinand.drive(0.5, gamepad1, y); // Standard - 0.84
+                else if (gamepad1.left_bumper) ferdinand.drive(0.1, gamepad1, y); // Standard - 0.4
+                else ferdinand.drive(0.3, gamepad1, y);
                 // Set normal speed to 0.5 at beginning of next season - for practice
 
                 // Drone controls
